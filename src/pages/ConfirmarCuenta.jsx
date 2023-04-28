677c762af373757a24c338d5f0474b959023b431
Stecklain <juanmstecklain@gmail.com>
2023-04-27 22:08:47 -0300

    first commit

---
+++ src/pages/ConfirmarCuenta.jsx
@@ -0,0 +1,12 @@
+
+const ConfirmarCuenta = () => {
+    return (
+        <>
+            <h1 className="text-sky-600 font-black text-6xl">Confirma Tu Cuenta Y Comienza A Crear Tus {''} 
+                <span className="text-slate-700">Proyectos</span>
+            </h1>
+        </>
+    )
+}
+
+export default ConfirmarCuenta
